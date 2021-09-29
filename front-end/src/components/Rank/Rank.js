e4da41323ddc42eb8cb589faf8ec022e80984b54
ycleo <leoyclee@gmail.com>
2021-09-30 00:13:25 +0800

    fix click display error

--- front-end/src/components/Rank/Rank.js
+++ front-end/src/components/Rank/Rank.js
@@ -5,7 +5,7 @@ const Rank = ({ name, entries }) => {
     return (
         <div>
             <div className='white f3'>
-                {`${name}, your current entry count is...`}    
+                {`${name}, your number of clicks is...`}    
             </div>
             <div className='white f1'>
                 {entries}    
